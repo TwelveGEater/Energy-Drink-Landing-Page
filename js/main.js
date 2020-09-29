794b5822d9b5fe1484a435b9bcc6a9b1ce59c228
dronkfc@gmail.com <dronkfc@gmail.com>
2020-09-29 13:51:37 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var owl = $('.owl-carousel');
+
+owl.owlCarousel({
+	center: true,
+	items: 2,
+	loop: true,
+	margin: 14,
+	dots: true,
+	dotsContainer: '.scrolling__menu',
+	responsive: {
+		0: {
+			center: true,
+			items: 2,
+			dots: false,
+			nav: false
+		},
+		600: {
+			items: 3
+		},
+		992: {
+			items: 4
+		},
+		1300: {
+			items: 5
+		},
+		1800: {
+			items: 7
+		}
+	}
+});
+
+$('#scrollingButtonLeft').click(function() {
+	owl.trigger('prev.owl.carousel');
+});
+
+$('#scrollingButtonRight').click(function() {
+	owl.trigger('next.owl.carousel');
+});
+
+$('#back-to-top').click(function() {
+	$('body,html').animate(
+		{
+			scrollTop: 0
+		},
+		400
+	);
+	return false;
+});
